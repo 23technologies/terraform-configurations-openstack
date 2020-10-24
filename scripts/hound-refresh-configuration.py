23ab37e15e3bd2113fa258932aec51e07a48d9cb
Christian Berendt <berendt@23technologies.cloud>
2020-10-24 22:07:58 +0200

    Move refresh-hound-config.py to scripts/
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

--- scripts/hound-refresh-configuration.py
+++ scripts/hound-refresh-configuration.py
@@ -22,5 +22,6 @@ for repository in gh.get_organization("gardener").get_repos():
     config["repos"][repository.full_name]["url"] = repository.clone_url
     config["repos"][repository.full_name]["ms-between-poll"] = 3600000
 
-with open("config.json", "w") as fp:
+with open("hound/service/configuration/config.json", "w") as fp:
     json.dump(config, fp, indent=4)
+    fp.write('\n')
